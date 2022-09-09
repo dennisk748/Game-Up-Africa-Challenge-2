ff7ca19e9be8fe3a4dc61d91c27736bc7d09dac6
dennisk748 <52379082+dennisk748@users.noreply.github.com>
2022-09-09 16:36:59 +0300

    1stCommit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float outsideVisible;
+    bool waitForSeconds = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(WaitForSeconds());
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !waitForSeconds)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            waitForSeconds = true;
+        }
+    }
+    IEnumerator WaitForSeconds()
+    {
+        if (waitForSeconds)
+        {
+            yield return new WaitForSeconds(2.5f);
+            waitForSeconds = false;
+        }
+    }
+}
